e8c8d92699e1124560a4d59365fdfd938967f11b
Al Shahariar Arafat Shawon <shahariararafat7739@gmail.com>
2025-07-22 17:53:25 +0600

    Day 1 of problem solving

---
+++ A_Say_Hello_With_C.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    char a[100];
+    cin>>a;
+    cout<<"Hello, "<<a<<endl;
+    return 0;
+}
---
+++ C_Next_Alphabet.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    char ch;
+    scanf("%c",&ch);
+    if(ch=='z')
+    {
+        printf("a");
+    }
+    else
+    {
+
+    printf("%c",ch+1);
+    }
+
+    return 0;
+}
